0271d52d30612f5d558f592a483dd6b48c738517
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-16 17:35:06 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,9 @@ def my_select(collection)
   counter = 0 
   new_collection = []
   while collection.length > counter 
-    new_collection << yield(collection[counter])
+    if yield(collection[counter]) == true
+      new_collection << collection[counter]
+    end 
     counter += 1 
   end 
   new_collection
